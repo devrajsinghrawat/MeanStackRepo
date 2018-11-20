6d84b181ad39c6358d4c05237868222964656b41
Devraj Singh <devraj.singh.rawat83@gmail.com>
2018-11-20 13:54:17 +0530

    update

--- gitSync/NodeExplore/webServer/app.js
+++ gitSync/NodeExplore/webServer/app.js
@@ -1,6 +1,22 @@
-let http = require('http');
+const http = require('http');
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello World \n')
-}).listen(1234, 'localhost');
+// http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' })
+//     res.end('Hello World \n')
+// }).listen(1234, 'localhost');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+})
+
+server.listen(3000);
+
+console.log('Listening at Port 3000 ...............');
