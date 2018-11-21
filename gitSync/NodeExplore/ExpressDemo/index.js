d75be8d1c6a82d6ed07cede5e40b99630336131f
Devraj Singh <devraj.singh.rawat83@gmail.com>
2018-11-21 10:10:58 +0530

    Updated POST PUT and Delete

--- gitSync/NodeExplore/ExpressDemo/index.js
+++ gitSync/NodeExplore/ExpressDemo/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 
 const app = express();
 
@@ -19,22 +20,52 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     // res.send(req.params.id);
-    const validate = courses.find(c => c.id === parseInt(req.params.id));
-    if (!validate) res.status(404).send(`Course with id ${req.params.id} does not found`);
-    res.send(validate);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send(`Course with id ${req.params.id} does not found`);
+
+    res.send(course);
 })
 
-
+// Post Operation
 app.post('/api/courses', (req, res) => {
+    const result = validateCourse(req.body);
+
+    if (result.error) return res.status(400).send(result.error);
+
     const course = {
-        id = courses.length + 1,
-        name = req.body.name
+        id: (courses.length + 1),
+        name: req.body.name
     }
 
     courses.push(course);
     res.send(course);
 })
 
+// Update Operation
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course, If not exist then error 404
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send(`Course with id ${req.params.id} does not found`);
+
+    const result = validateCourse(req.body);
+
+    if (result.error) return res.status(400).send(result.error);
+
+    course.name = req.body.name;
+    res.send(course);
+})
+
+// Delete Operation 
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course, If not exist then error 404
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send(`Course with id ${req.params.id} does not found`);
+
+    const index = courses.indexOf(course);
+    courses.splice(course, index)
+
+    res.send(course);
+})
 
 // app.get('/api/posts/:year/:month', (req, res) => {
 //     res.send(req.params);
@@ -46,3 +77,11 @@ app.post('/api/courses', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Port ${port} is listening .......`));
+
+function validateCourse(course) {
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course, schema);
+}
